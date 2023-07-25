beec05893bdd83acded9e4a52327e06745f7dace
Cari Costa <cari.costa90@gmail.com>
2023-07-24 21:26:15 -0300

    Trabajamos iteraciones de ArrayList, add, get, set, remove

--- MascotaAPP/src/mascotaapp/MascotaAPP.java
+++ MascotaAPP/src/mascotaapp/MascotaAPP.java
@@ -1,18 +1,53 @@
 package mascotaapp;
 
+import java.util.Scanner;
 import mascotaapp.services.MascotaService;
 
 public class MascotaAPP {
 
     public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
 
         MascotaService ms = new MascotaService();
-
-        ms.fabricaMascotas(2);
+        System.out.println("Fabrica automatica");
+        ms.fabricaMascotas(3);
         ms.mostrarMascotas();
-        ms.crearMascota();
+        System.out.println("Crear mascota a mano");
+        ms.fabricaMascTeclado(1);
         ms.mostrarMascotas();
+        System.out.println("Actualizar mascota con Get indicando el indice, le asignara el apodo Roberto");
+        ms.actualizarMascotaGet(2);
+        ms.mostrarMascotas();
+        System.out.println("Actualizar mascota indicando el indice y cargando los datos nuevos por input");
+        ms.actualizarMascotaSet(0);
+        ms.mostrarMascotas();
+        System.out.println("--------------------------------");
+        System.out.println("Eliminamos una mascota por indice");
+        ms.eliminarMascota(3);
+        ms.mostrarMascotas();        
+        System.out.println("Trabajamos con iteracion eliminando un elemento de la lista buscando por nombre");
+        System.out.println("Ingresa el nombre de la mascota que buscamos eliminar");
+        ms.eliminarMascotaPorNombre(leer.next());
+        ms.mostrarMascotas();
+        
+        /*
+         * como convertir un array a arrayList
+         * 
+         *CREAMOS EL ARRAY
+        String[] nombres = {"Pepe", "Pepa"};
+        
+         * Y LO PASAMOS A ARRAY LIST POR MEDIO DEL METODO asList
+        List <String> nombresList = new ArrayList(Arrays.asList(nombres));
 
+        Tambien se puede agregar un arrayList a otro ArrayList
+        
+        List<String> nombresList2 = new ArrayList();
+        nombresList2.addAll(nombresList);
+         */
+        
+        
+       
     }
 
 }
--- MascotaAPP/src/mascotaapp/services/MascotaService.java
+++ MascotaAPP/src/mascotaapp/services/MascotaService.java
@@ -1,67 +1,97 @@
-
 package mascotaapp.services;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 import mascotaapp.entities.Mascota;
 
-
 public class MascotaService {
-    
-    private Scanner leer; 
+
+    private Scanner leer;
     private ArrayList<Mascota> mascotas;
     private Mascota mascota;
-    
-    public MascotaService(){
+
+    public MascotaService() {
         this.leer = new Scanner(System.in).useDelimiter("\n");
         this.mascotas = new ArrayList();
         this.mascota = new Mascota();
     }
-    
-    
-    
-    public Mascota crearMascota(){
-        
+
+    public Mascota crearMascota() {
+
         System.out.println("Ingresa el nombre de la mascota");
-        mascota.setNombre(leer.next());
-        
+        String nombre = leer.next();
+
         System.out.println("Ingresa el apodo de la mascota");
-        mascota.setApodo(leer.next());
-        
+        String apodo = leer.next();
+
         System.out.println("Ingresa tipo de mascota");
-        mascota.setTipo(leer.next());
-        
-        mascotas.add(mascota);
-     
-        return mascota;
-        
+        String tipo = leer.next();
+
+        return new Mascota(nombre, apodo, tipo);
+
     }
-    
-    public void mostrarMascotas(){
-        System.out.println("La lista actual de mascotas es: "); 
+
+    public void mostrarMascotas() {
+        System.out.println("La lista actual de mascotas es: ");
         for (Mascota mascota1 : mascotas) {
             System.out.println(mascota1.toString());
-        }        
+        }
         System.out.println("La cantidad = " + mascotas.size());
     }
-    
+
     /**
-     * 
+     *
      * @param cantidad es la cantidad de mascotas a crear y agregar a la lista
+     *
      */
-    
-    public void fabricaMascotas(int cantidad){
-        
+    //crea mascotas automatizado, todas con el mismo nombre y datos en general
+    //veremos aqui ejemplos de agregar elementos al arrayList por medio del metodo "add"
+    public void fabricaMascotas(int cantidad) {
+
         for (int i = 0; i < cantidad; i++) {
-            mascotas.add(new Mascota(("Mascota"+i), "Chiqui", "Perro"));
+            mascotas.add(new Mascota(("Mascota" + i), "Chiqui", "Perro"));
         }
     }
-    
-    public void fabricaMascTeclado(int cantidad){
+
+    //crea mascotas con el input asignado en crearMascota
+    public void fabricaMascTeclado(int cantidad) {
         for (int i = 0; i < cantidad; i++) {
-            crearMascota();
+            mascotas.add(crearMascota());
         }
     }
-    
+
+    //actualizar objetos
+    //TODO aniadir Try y Catch para evitar errores cuando el indice ingresado no es valido
+    //usando get:
+    public void actualizarMascotaGet(int index) {
+        Mascota m = mascotas.get(index);
+        m.setApodo("Roberto");
+    }
+
+    //usando set
+    public void actualizarMascotaSet(int index) {
+        Mascota m = crearMascota();
+        //aca lo que estamos haciendo es insertar la mascota ingresada ahora en el indice indicado en el set.
+        //este crea un nuevo objeto y pisa en base al indice
+        mascotas.set(index, m);
+    }
+
+    //eliminar datos
+    public void eliminarMascota(int index) {
+        mascotas.remove(index);
+        //para remove con objeto lo que se debe pasar es la referencia del objeto en la memoria
+    }
+
+    //eliminar mascota por nombre iterando en la lista 
+    public void eliminarMascotaPorNombre(String nombre) {
+
+        for (int i = 0; i < mascotas.size(); i++) {
+            Mascota m = mascotas.get(i);
+            if (m.getNombre().equals(nombre)) {
+                mascotas.remove(i);
+            }
+        }
+
+    }
 
 }
